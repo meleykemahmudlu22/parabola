f28e72c906edac3f096f8198a365e1992e25c546
meleykemahmudlu22 <meleykemahmudlu03@gmail.com>
2026-05-23 11:33:03 +0400

    cothing box

--- parabolasite/src/App.jsx
+++ parabolasite/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './assets/components/Header/Header'
 import Katalog from './assets/components/katalog/Katalog'
+import Cothing from './assets/components/clothing/Cothing'
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Katalog/>
+      <Cothing/>
     </div>
   )
 }
---
+++ parabolasite/src/assets/components/clothing/Cothing.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { GoArrowRight } from "react-icons/go";
+import "./clothing.css"
+
+const Cothing = () => {
+  return (
+    <div>
+        <div className="cothingcontainer">
+         
+           <div className="box">
+               <h1>Geyimlər</h1>
+             <div className="cothingboxcontainer">
+                
+               <div className="cothingbox">
+                <div className="colorbtn">
+                    <button></button>
+                </div>
+               <div className="cothingimg">
+                <img src="https://gunnandmoore.playwiththebest.com/media/catalog/product/cache/ec4e4c8893a2305e77afd20d2909bacb/7/0/7047_teknik_slipover_white_1.png" alt="" />
+               </div>
+               <div className="cothingtext">
+                <div className="cothingtoptext">
+                   <h3>Klassik Ağ Köynək</h3>
+                </div>
+                <div className="cothingbtn">
+                    <button>M</button>
+                    <button>Kişi</button>
+                    <button>Rəsmi</button>
+                </div>
+                <div className="cothingbutton">
+                    <button>Sına<GoArrowRight /></button>
+                </div>
+               </div>
+               </div>
+               <div className="cothingbox">
+                <div className="colorbtn">
+                    <button></button>
+                </div>
+               <div className="cothingimg">
+                <img src="https://gunnandmoore.playwiththebest.com/media/catalog/product/cache/ec4e4c8893a2305e77afd20d2909bacb/7/0/7047_teknik_slipover_white_1.png" alt="" />
+               </div>
+               <div className="cothingtext">
+                <div className="cothingtoptext">
+                   <h3>Klassik Ağ Köynək</h3>
+                </div>
+                <div className="cothingbtn">
+                    <button>M</button>
+                    <button>Kişi</button>
+                    <button>Rəsmi</button>
+                </div>
+                <div className="cothingbutton">
+                    <button>Sına<GoArrowRight /></button>
+                </div>
+               </div>
+               </div>
+               <div className="cothingbox">
+                <div className="colorbtn">
+                    <button></button>
+                </div>
+               <div className="cothingimg">
+                <img src="https://gunnandmoore.playwiththebest.com/media/catalog/product/cache/ec4e4c8893a2305e77afd20d2909bacb/7/0/7047_teknik_slipover_white_1.png" alt="" />
+               </div>
+               <div className="cothingtext">
+                <div className="cothingtoptext">
+                   <h3>Klassik Ağ Köynək</h3>
+                </div>
+                <div className="cothingbtn">
+                    <button>M</button>
+                    <button>Kişi</button>
+                    <button>Rəsmi</button>
+                </div>
+                <div className="cothingbutton">
+                    <button>Sına<GoArrowRight /></button>
+                </div>
+               </div>
+               </div>
+            </div>
+           </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Cothing
